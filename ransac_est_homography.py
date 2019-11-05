8bbeae7a6e85ed3ec371b72caaa723ee9494b521
pls19961219@gmail.com <pls19961219@gmail.com>
2019-11-04 21:03:26 -0500

    ransac.py updated **vecterized

--- ransac_est_homography.py
+++ ransac_est_homography.py
@@ -6,9 +6,9 @@
 
 '''
   File clarification:
-    Use a robust method (RANSAC) to compute a homography. Use 4-point RANSAC as 
+    Use a robust method (RANSAC) to compute a homography. Use 4-point RANSAC as
     described in class to compute a robust homography estimate:
-    - Input x1, y1, x2, y2: N × 1 vectors representing the correspondences feature coordinates in the first and second image. 
+    - Input x1, y1, x2, y2: N × 1 vectors representing the correspondences feature coordinates in the first and second image.
                             It means the point (x1_i , y1_i) in the first image are matched to (x2_i , y2_i) in the second image.
     - Input thresh: the threshold on distance used to determine if transformed points agree.
     - Outpuy H: 3 × 3 matrix representing the homograph matrix computed in final step of RANSAC.
@@ -20,34 +20,49 @@ import random
 from math import sqrt
 from est_homography import est_homography
 
-def ransac_est_homography(x1, y1, x2, y2, thresh): #x1, y1, x2, y2 should be numpy array
-  k = 1000
-  N = len(x1)
-  H = np.zeros([3,3])
-  inlier_ind = np.zeros([N,])
-  for i in range(k):
-    curr_inlier_ind = np.zeros([N,])
-    sample = random.sample(list(range(N)), 4)
-    x = x1[sample]
-    y = y1[sample]
-    X = x2[sample]
-    Y = y2[sample]
-    curr_H = est_homography(x, y, X, Y)
 
-    for feature in range(N):  #need to be simplified
-      pred_x = curr_H[0,0]*x1[feature] + curr_H[0,1]*y1[feature] + curr_H[0,2]
-      pred_y = curr_H[1,0]*x1[feature] + curr_H[1,1]*y1[feature] + curr_H[1,2]
-      pred_z = curr_H[2,0]*x1[feature] + curr_H[2,1]*y1[feature] + curr_H[2,2]
-      pred_x = pred_x/pred_z
-      pred_y = pred_y/pred_z
-      dist = sqrt((pred_x-x2[feature])**2 + (pred_y-y2[feature])**2)
-      if dist < thresh:
-        curr_inlier_ind[feature] = 1
-      else:
-        curr_inlier_ind[feature] = 0
+def ransac_est_homography(x1, y1, x2, y2, thresh):  # x1, y1, x2, y2 should be numpy array
+    k = 1000
+    N = len(x1)
+    H = np.zeros([3, 3])
+    ones = np.ones([N, ])
+    inlier_ind = np.zeros([N, ])
+    for i in range(k):
+        curr_inlier_ind = np.zeros([N, ])
+        sample = random.sample(list(range(N)), 4)
+        x = x1[sample]
+        y = y1[sample]
+        X = x2[sample]
+        Y = y2[sample]
+        curr_H = est_homography(x, y, X, Y)
 
-    if sum(curr_inlier_ind) > sum(inlier_ind):
-      inlier_ind = curr_inlier_ind
-      H = curr_H
+        img1_coor_matrix = np.vstack((x1.flatten(), y1.flatten(), ones))
+        img2_coor_matrix = np.vstack((x2.flatten(), y2.flatten()))
 
-  return H, inlier_ind
+        img2_coor_matrix_hat = curr_H @ img1_coor_matrix
+
+        Lambda = img2_coor_matrix_hat[0:2, :] - img2_coor_matrix
+        Sigma = Lambda * Lambda
+        d = np.sqrt(Sigma[0, :] + Sigma[1, :])
+        curr_inlier_ind = (d - thresh) < 0
+
+        if sum(curr_inlier_ind) > sum(inlier_ind):
+            inlier_ind = curr_inlier_ind
+            H = curr_H
+        # for feature in range(N):  #need to be simplified
+        #   pred_x = curr_H[0,0]*x1[feature] + curr_H[0,1]*y1[feature] + curr_H[0,2]
+        #   pred_y = curr_H[1,0]*x1[feature] + curr_H[1,1]*y1[feature] + curr_H[1,2]
+        #   pred_z = curr_H[2,0]*x1[feature] + curr_H[2,1]*y1[feature] + curr_H[2,2]
+        #   pred_x = pred_x/pred_z
+        #   pred_y = pred_y/pred_z
+        #   dist = sqrt((pred_x-x2[feature])**2 + (pred_y-y2[feature])**2)
+        #   if dist < thresh:
+        #     curr_inlier_ind[feature] = 1
+        #   else:
+        #     curr_inlier_ind[feature] = 0
+
+        # if sum(curr_inlier_ind) > sum(inlier_ind):
+        #   inlier_ind = curr_inlier_ind
+        #   H = curr_H
+
+    return H, inlier_ind
